353d660bde7aa1178255c89375bdbea6d5cf2139
Erudite885 <erudite885@gmail.com>
2023-12-05 20:51:19 +1300

    form submithandler function fixed

--- src/components/PodForm.tsx
+++ src/components/PodForm.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const PodForm: React.FC = () => {
+  const [email, setEmail] = useState<string>('')
+
+  // const emailChangeHandler = (e: React.ChangeEventHandler<HTMLInputElement>) => {
+  //   setEmail(e.target.value);
+  // }
+
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  }
+
+  console.log(email)
   return (
-    <form className='flex flex-col py-5 gap-5'>
-      
-      <input className='py-2 px-4 rounded-[2rem]' type="email" placeholder='Email address' />
-      <button type='submit' className='text-emerald-950 bg-emerald-700 rounded-[2rem] font'>Request Access</button>
+    <form onSubmit={submitHandler} className='flex flex-col py-5 gap-5'>
+      <input className='py-2 px-4 rounded-[2rem]' type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email address' />
+      <button type='submit' className='cursor-pointer text-emerald-950 bg-emerald-700 rounded-[2rem] font'>Request Access</button>
     </form>
   )
 }
